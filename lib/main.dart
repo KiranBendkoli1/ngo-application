d6134acb6944eca4fb907d52c92dd74f27944002
KiranBendkoli1 <bendkolikiran@kbtcoe.org>
2023-02-05 17:23:27 +0530

    installed dependencies and set up firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 // ignore_for_file: use_full_hex_values_for_flutter_colors, prefer_const_constructors
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:ngo_app/pages/landing_page.dart';
+import 'package:ngo_app/provider/auth_provider.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const NGOApplication());
 }
 
@@ -12,18 +17,25 @@ class NGOApplication extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Color(0xfff0b5d0b),
-        primarySwatch: Colors.green,
-        appBarTheme: AppBarTheme(
-          backgroundColor: Color(0xFF0B5D0B),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AuthProvider(),
         ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Color(0xfff0b5d0b),
+          primarySwatch: Colors.green,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Color(0xFF0B5D0B),
+          ),
+        ),
+        title: "NGO Application",
+        home: LandingPage(),
       ),
-      title: "NGO Application",
-      home: LandingPage(),
     );
   }
 }
